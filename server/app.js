ab0323b27780eb985aad13cfb74e95da06f426c1
shavinda <arunshavinda@icloud.com>
2024-01-07 20:13:21 +0530

    refactoring final

--- server/app.js
+++ server/app.js
@@ -1,10 +1,7 @@
-//states
 const path = require("path")
 const { Server } = require("socket.io")
 
 const express = require("express")
-const mongoose = require("mongoose")
-const jwt = require("jsonwebtoken")
 const multer = require("multer")
 const cors = require("cors")
 
@@ -18,9 +15,10 @@ app.use(cors())
 
 require("./utils/conection")
 
-app.get("/", (req, res) => {
-  res.send("hello there")
-})
+const routes = require("./routes/route")
+
+//get all the routes from route.js and use them
+app.use("", routes)
 
 //store images
 
@@ -41,176 +39,6 @@ app.post("/upload", upload.single("product"), (req, res) => {
     success: 1,
     image_url: `http://localhost:${PORT}/images/${req.file.filename}`,
   })
-})
-
-// create a product schema
-const { Product } = require("./models/Products")
-
-app.post("/addproduct", async (req, res) => {
-  let products = await Product.find({})
-  let id
-  if (products.length > 0) {
-    let latest_product = products.slice(-1)[0]
-    id = latest_product.id + 1
-  } else {
-    id = 1
-  }
-
-  const { name, image, category, new_price, old_price, description } = req.body
-  const product = new Product({
-    id: id,
-    name: name,
-    image: image,
-    category: category,
-    new_price: new_price,
-    old_price: old_price,
-    description: description,
-  })
-  console.log(product)
-  await product.save()
-  console.log("saved")
-
-  res.status(200).json({ success: true, name: name })
-})
-
-app.post("/removeproduct", async (req, res) => {
-  await Product.findOneAndDelete({ id: req.body.id })
-  console.log("product deleted")
-  res.status(200).json({ success: true, name: req.body.name })
-})
-
-app.get("/allproducts", async (req, res) => {
-  let products = await Product.find({})
-  console.log("fetched")
-  res.send(products)
-})
-
-//user schema
-
-const { Users } = require("./models/User")
-
-//create user
-app.post("/signup", async (req, res) => {
-  let check = await Users.findOne({ email: req.body.email })
-  if (check) {
-    return res
-      .status(401)
-      .json({ success: false, error: "email already in use" })
-  }
-  let cart = {}
-  for (let i = 0; i < 300; i++) {
-    cart[i] = 0
-  }
-  const user = new Users({
-    name: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    cartData: cart,
-  })
-  await user.save()
-
-  const data = {
-    user: {
-      id: user.id,
-    },
-  }
-
-  const token = jwt.sign(data, "secret")
-  res.json({ success: true, token })
-})
-
-app.post("/login", async (req, res) => {
-  let user = await Users.findOne({ email: req.body.email })
-  if (user) {
-    const conf_pass = req.body.password === user.password
-    if (conf_pass) {
-      const data = {
-        user: {
-          id: user.id,
-        },
-      }
-      const token = jwt.sign(data, "secret")
-      res.status(200).json({ success: true, token })
-    } else {
-      res.status(400).json({ success: false, errors: "wrong password" })
-    }
-  } else {
-    res.json({ success: false, errors: "email not found" })
-  }
-})
-
-//new items
-
-app.get("/newproducts", async (req, res) => {
-  let products = await Product.find({})
-  //get the most recent 8 products
-  let new_products = products.slice(1).slice(-8)
-  console.log("new items fetched")
-  res.status(200).send(new_products)
-})
-
-app.get("/bestselling", async (req, res) => {
-  // getting the most recent listed printes
-  let products = await Product.find({ category: "3dprinters" })
-  let best_printers = products.slice(0, 4)
-  console.log("popular printers fetched")
-  res.status(200).send(best_printers)
-})
-
-//add to cart
-
-// middleware to get the user
-const fetchUser = async (req, res, next) => {
-  const token = req.header("auth-token")
-  if (!token) {
-    return res
-      .status(401)
-      .send({ errors: "Please login to validate : token not valid" })
-  } else {
-    try {
-      const data = jwt.verify(token, "secret")
-      req.user = data.user
-      next()
-    } catch (error) {
-      res.status(401).send({ errors: "token not valid" })
-    }
-  }
-}
-
-app.post("/addtocart", fetchUser, async (req, res) => {
-  console.log("added", req.body.itemId)
-
-  let userData = await Users.findOne({ _id: req.user.id })
-  userData.cartData[req.body.itemId] += 1
-  await Users.findOneAndUpdate(
-    { _id: req.user.id },
-    { cartData: userData.cartData }
-  )
-  res.send("item added to cart")
-})
-
-//remove cart data
-app.post("/removefromcart", fetchUser, async (req, res) => {
-  let userData = await Users.findOne({ _id: req.user.id })
-  console.log("removed", req.body.itemId)
-
-  if (userData.cartData[req.body.itemId] > 0) {
-    userData.cartData[req.body.itemId] -= 1
-  }
-
-  await Users.findOneAndUpdate(
-    { _id: req.user.id },
-    { cartData: userData.cartData }
-  )
-  res.send("item removed from cart")
-})
-
-//get cart data
-
-app.post("/getcart", fetchUser, async (req, res) => {
-  console.log("get cart items")
-  let userData = await Users.findOne({ _id: req.user.id })
-  res.status(200).json(userData.cartData)
 })
 
 // server api
@@ -241,4 +69,5 @@ io.on("connection", (socket) => {
   })
 })
 
+const { Product } = require("./models/Products")
 module.exports = { app, Product }
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,2 +1,56 @@
 const express = require("express")
 const router = express.Router()
+const jwt = require("jsonwebtoken")
+
+const {
+  sayhello,
+  addproduct,
+  removeproduct,
+  allproducts,
+  signup,
+  bestselling,
+  login,
+  newproducts,
+  addtocart,
+  removefromcart,
+  getcart,
+} = require("../controllers/controller")
+
+router.get("/", sayhello)
+router.post("/addproduct", addproduct)
+router.post("/removeproduct", removeproduct)
+router.get("/allproducts", allproducts)
+
+router.post("/signup", signup)
+router.post("/login", login)
+
+//new items
+router.get("/newproducts", newproducts)
+router.get("/bestselling", bestselling)
+
+//add to cart
+// middleware to get the user
+const fetchUser = async (req, res, next) => {
+  const token = req.header("auth-token")
+  if (!token) {
+    return res
+      .status(401)
+      .send({ errors: "Please login to validate : token not valid" })
+  } else {
+    try {
+      const data = jwt.verify(token, "secret")
+      req.user = data.user
+      next()
+    } catch (error) {
+      res.status(401).send({ errors: "token not valid" })
+    }
+  }
+}
+
+router.post("/addtocart", fetchUser, addtocart)
+//remove cart data
+router.post("/removefromcart", fetchUser, removefromcart)
+//get cart data
+
+router.post("/getcart", fetchUser, getcart)
+module.exports = router
