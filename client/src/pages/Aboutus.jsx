6b0952d968927925ac857c86da1b1bd0507f8f90
umedha33 <umedharaj@gmail.com>
2024-01-08 13:17:43 +0530

    my account restructure

--- client/src/pages/Aboutus.jsx
+++ client/src/pages/Aboutus.jsx
@@ -7,25 +7,16 @@ const Aboutus = () => {
     <div className='aboutus'>
       <div className="aboutus-container">
         <h1>About Us</h1>
-
-
-
+        
         <div className="divider">
-
           <div className="about-info">
             <h2>Who We Are?</h2>
             <p>Since its inception, Axis Sri Lanka has emerged as a leading provider of cutting-edge 3D printers, 3D models, printer parts, and related accessories. What began as a vision has now evolved into our dedicated passion, and we are thrilled to share our expertise with you. Over the years, we have garnered a reputation for excellence and have served numerous satisfied customers. Our commitment to the world of 3D printing drives us every day, and we take pride in expanding our product range to meet all your printing needs. At Axis Sri Lanka, we understand that exceptional service is built on a foundation of knowledgeable individuals with industry experience. Our team comprises skilled and dedicated professionals who are genuinely committed to ensuring that your experience with us is seamless, convenient, and fulfilling. We invite you to visit our store, connect with us, and explore the possibilities of 3D printing. If you have any inquiries, feel free to reach out to us at <span>axis.srilanka@gmail.com</span>. We look forward to being a part of your 3D printing journey.</p>
-
           </div>
-
           <div className="aboutus-fields">
             <img src={about_image} alt="about-us-image" />
           </div>
-
         </div>
-
-
-
       </div>
     </div>
   )
--- client/src/pages/Myaccount.jsx
+++ client/src/pages/Myaccount.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import "./CSS/Myaccount.css";
-import { ShopContext } from "../../Context/ShopContext";
+import { ShopContext } from "../Context/ShopContext"
+import { useState } from "react";
+import { useContext } from "react";
 
 const Myaccount = () => {
   const { changepass } = useContext(ShopContext);
   const [oldpassword, setoldpassword] = useState("");
   const [newpassword, setnewpassword] = useState("");
+  const email = "test email";
+
   return (
     <div className="myaccount">
-      <span>emai: ${email}</span>
-      <div>
-        <input
-          type="text"
-          name="oldpassword"
-          onChange={(event) => setoldpassword(event.target.value)}
-          placeholder="old password"
-        />
-        <input
-          type="text"
-          name="newpassword"
-          onChange={(event) => setnewpassword(event.target.value)}
-          placeholder="new password"
-        />
-        <button onClick={changepass(oldpassword, newpassword)}>
-          Change Password
-        </button>
+      <div className="myaccount-container">
+        <h1>My Account</h1>
+        <h2>Your Email: {email}</h2>
+        <hr />
+        <div className="myaccount-info">
+          <h3>Update your password here,</h3>
+          
+          <p>Enter old password (current)</p>
+          <input type="text" name="oldpassword" onChange={(event) => setoldpassword(event.target.value)} placeholder="Old password" />
+          
+          <p>Enter new password</p>
+          <input type="text" name="newpassword" onChange={(event) => setnewpassword(event.target.value)} placeholder="New password" />
+          
+          <div className="submit-btn">
+            <button onClick={changepass(oldpassword, newpassword)}>Change Password</button>
+          </div>
+        </div>
       </div>
     </div>
   );
