7676e6318207d9706c3192134f5240402e67ad2b
umedha33 <umedharaj@gmail.com>
2024-01-08 14:54:53 +0530

    admin source cleaned

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/axis-logo.png'
-import navProfile from '../../assets/nav-profile.svg'
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img className='nav-logo' src={logo} alt="" />
-      <h1>Admin Panel</h1>    
+      <Link to='/'>
+        <img className='nav-logo' src={logo} alt="" />
+      </Link>
+      <h1>Admin Panel</h1>
     </div>
   )
 }
